fb7032dd4dba8d4bfcdb1f544ca5438af3561a29
何凌强 <2633865300@qq.com>
2025-03-16 21:39:48 +0800

    优化

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
     vue(),
     // vueDevTools(),
   ],
+  publicPath: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -25,6 +26,23 @@ export default defineConfig({
       }
 
     }
+  },
+  build: {
+    chunkSizeWarningLimit: 1000, // 提高警告阈值（临时方案）
+    assetsInlineLimit: 4096,     // 4KB 以下资源转为 DataURL
+    cssCodeSplit: true,         // 启用 CSS 代码分割
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]',// 哈希化静态资源
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // 将每个第三方库独立分包（如 lodash → lodash.js）
+            return id.toString().split('node_modules/')[1].split('/')[0]
+          }
+          // 将业务代码中的公共模块分组
+          if (id.includes('src/utils')) return 'common-utils'
+        }
+      }
+    }
   }
-
 })
